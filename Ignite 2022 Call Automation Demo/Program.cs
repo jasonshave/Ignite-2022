4dfb8fff1384cb744fe005c727f80db48ef3c5e8
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2022-09-13 14:03:39 -0600

    Add project files.

---
+++ Ignite 2022 Call Automation Demo/Program.cs
@@ -0,0 +1,60 @@
+using Azure.Communication;
+using Azure.Communication.CallingServer;
+using Azure.Messaging;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton(new CallAutomationClient(builder.Configuration["ConnectionString"]));
+
+var app = builder.Build();
+
+app.MapPost("/api/calls/{contextId}", async (
+    HttpRequest httpRequest,
+    [FromRoute] string contextId,
+    CallAutomationClient callAutomationClient,
+    ILogger<Program> logger) =>
+{
+    var mayPhoneNumber = "+17809669598";
+    try
+    {
+        var cloudEvents = await httpRequest.ReadFromJsonAsync<CloudEvent[]>();
+        foreach (var cloudEvent in cloudEvents)
+        {
+            var @event = CallAutomationEventParser.Parse(cloudEvent);
+            logger.LogInformation($"CorrelationId: {@event.CorrelationId} | CallConnectionId: {@event.CallConnectionId}");
+
+            if (@event.GetType() == typeof(CallConnected))
+            {
+                // invoke recognize api
+                var recognizeOptions = new CallMediaRecognizeDtmfOptions(new PhoneNumberIdentifier(mayPhoneNumber))
+                {
+                    MaxTonesToCollect = 1,
+                    InitialSilenceTimeout = TimeSpan.FromSeconds(10),
+                    InterToneTimeout = TimeSpan.FromSeconds(5),
+                    InterruptPrompt = true,
+                    InterruptCallMediaOperation = true,
+                    Prompt = new FileSource(new Uri("https://callingstoragequeue.blob.core.windows.net/ivr/contoso-airlines-main-menu.wav")),
+                    OperationContext = "OutboundFlightChange",
+                    StopTones = new [] { DtmfTone.Pound }
+                };
+
+                await callAutomationClient.GetCallConnection(@event.CallConnectionId).GetCallMedia().StartRecognizingAsync(recognizeOptions);
+            }
+
+            if (@event.GetType() == typeof(RecognizeCompleted))
+            {
+                var recognizeCompleted = @event as RecognizeCompleted;
+                
+            }
+        }
+    }
+    catch (InvalidOperationException)
+    {
+        
+    }
+
+    return Results.Ok();
+}).Produces(StatusCodes.Status200OK);
+
+app.Run();
---
+++ OutboundDialer/Program.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Communication;
+using Azure.Communication.CallingServer;
+
+var callAutomationClient =
+    new CallAutomationClient(
+        "endpoint = https://jassha.communication.azure.com/;accesskey=ZLjtIBwWyDhWRoMguG+YZScz5YugDV7prAwzzi6QyK14YXtOiGqXEM4hgKyyq7Vj2xY8XHURX1q0MWMoNDDJnA==");
+
+var callbackUri = new Uri($"https://e196-75-155-253-232.ngrok.io/api/calls/{Guid.NewGuid()}");
+var source = new CommunicationUserIdentifier("8:acs:eba32226-8a75-47dc-afa3-cbbe8e84bc95_5bb7b304-4582-47d4-a0c1-1f763b4b9ecd");
+var callSource = new CallSource(source)
+{
+    CallerId = new PhoneNumberIdentifier("+18336392154")
+};
+var target = new List<CommunicationIdentifier> { new PhoneNumberIdentifier("+17809669598") };
+
+await callAutomationClient.CreateCallAsync(new CreateCallOptions(callSource, target, callbackUri));
