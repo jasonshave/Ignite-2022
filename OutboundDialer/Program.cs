054ff08f72035995b27f1850669d5dadc203bdea
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2022-09-27 11:29:51 -0600

    rename folders

--- OutboundDialer/Program.cs
+++ OutboundDialer/Program.cs
@@ -6,11 +6,12 @@ var connectionString = "[INSERT_CONNECTION_STRING_HERE]";
 var callerIdNumber = "[INSERT_ACS_NUMBER_HERE]";
 var targetPhoneNumber = "[INSERT_DESTINATION_PSTN_NUMBER_HERE]";
 var callbackHost = "[INSERT_HOST_HERE]";
+var applicationId = "[INSERT_ACS_ID_HERE]";
 #endregion
 
 var callAutomationClient = new CallAutomationClient(connectionString);
 var callbackUri = new Uri($"{callbackHost}/api/calls/{Guid.NewGuid()}");
-var sourceUserId = new CommunicationUserIdentifier("8:acs:cb089178-54ed-4d9e-9763-49510601f789_5bb7b304-4582-47d4-a0c1-1f763b4b9ecd");
+var sourceUserId = new CommunicationUserIdentifier(applicationId);
 var callSource = new CallSource(sourceUserId)
 {
     CallerId = new PhoneNumberIdentifier(callerIdNumber)
